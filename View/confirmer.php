4be911570307bfc9dac4e78ee4b98e6cb07015f4
christian1236 <51271240+christian1236@users.noreply.github.com>
2021-11-07 19:18:48 +0000

    Delete View directory

--- View/confirmer.php
+++
@@ -1,8 +0,0 @@
-<form action="Model/supprimer.php" method="post">
-    Are you sure?
-    <input type="submit" name="confirm" value="Yes">
-    <input type="submit" name="confirm" value="No">
-
-    <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-    <input type="hidden" name="referer" value="<?php echo $_SERVER['HTTP_REFERER']; ?>">
-</form>
