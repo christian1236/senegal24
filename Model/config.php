eab545ba84581ebd5afe4aa7b23d2b39f85dfe5e
christian1236 <51271240+christian1236@users.noreply.github.com>
2021-11-07 19:18:39 +0000

    Delete Model directory

--- Model/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-   $bdd = new PDO('mysql:host=localhost;dbname=mglsi_news;charset=utf8', 'root', 'root');
-?>
--- Model/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $articles = $bdd->query('SELECT * FROM Article ORDER BY dateCreation DESC LIMIT 0,3');
-    $categories = $bdd->query('SELECT * FROM Categorie');
-?>
